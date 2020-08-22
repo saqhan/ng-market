be66a3ede0c3bb9eeb265817cd31fe1a3e0d84d5
saqhan <saqhan1@mail.ru>
2020-08-22 17:54:44 +0300

    fix

--- src/app/shared/ng-cnt-market-header/ng-cnt-market-header.component.ts
+++ src/app/shared/ng-cnt-market-header/ng-cnt-market-header.component.ts
@@ -1,59 +1,54 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {MarketStoreService} from "../../market-store.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { MarketStoreService } from '../../market-store.service';
 
 @Component({
   selector: 'app-ng-cnt-market-header',
   templateUrl: './ng-cnt-market-header.component.html',
-  styleUrls: ['./ng-cnt-market-header.component.scss']
+  styleUrls: ['./ng-cnt-market-header.component.scss'],
 })
 export class NgCntMarketHeaderComponent implements OnInit {
-
   constructor(private marketStore: MarketStoreService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  @Input() base;
+  @Input() firstPage: boolean;
 
-  public firstPage = false;
 
   public getLogo() {
     return this.marketStore.logo;
   }
 
   public getNumberHeader() {
-    return this.marketStore.numberHeader
+    return this.marketStore.numberHeader;
   }
 
   /* Получаем данные для топ бара */
   getTopBar() {
-    return this.marketStore.getTopBar()
+    return this.marketStore.getTopBar();
   }
 
   /* Получаем данные для body header */
   getBodyHeader() {
-    return this.marketStore.getBodyHeader()
+    return this.marketStore.getBodyHeader();
   }
-
 
   //
   public getLoginUser() {
-    return this.marketStore.getLoginUser()
+    return this.marketStore.getLoginUser();
   }
 
   //
   public getNavBar() {
-    return this.marketStore.getNavBar()
+    return this.marketStore.getNavBar();
   }
 
   //
   public getSelectShops() {
-    return this.marketStore.getSelectShops()
+    return this.marketStore.getSelectShops();
   }
 
   //
   public getLeftMenuCatalog() {
-    return this.marketStore.getLeftMenuCatalog()
+    return this.marketStore.getLeftMenuCatalog();
   }
-
 }
